9e173e507c57d57a276970ced9ba2c5e0481c0ab
RomanKiselev91 <www.ksel@gmail.com>
2025-06-16 23:03:48 +0300

    Add files via upload

---
+++ Hello_cat.py
@@ -0,0 +1,32 @@
+import requests
+
+access_token = 'y0__xC08su4BhjblgMg0cj24BJE4l5nQiqWXb8TVb-ViZH8oMP5Aw'
+URL = "https://cloud-api.yandex.net/v1/disk/resources"
+URL_UPLOAD = 'https://cloud-api.yandex.net/v1/disk/resources/upload?'
+headers = {"Authorization": f"OAuth {access_token}"}
+URL_image = 'https://cataas.com/cat/says/'
+
+def create_folder(path):
+    requests.put(f'{URL}?path={path}', headers=headers)
+    return path
+
+def save_yadisk(folder, text):
+    response = requests.post(URL_UPLOAD, headers=headers,
+                                params={'path': f'{folder}/{text+'.jpg'}', 'url': f'{URL_image + text}'})
+
+def save_loc(text):
+    response = requests.get(URL_image + text)
+    with open(text + '.jpg', 'wb') as file:
+        file.write(response.content)
+def start_prog():
+    text = input('enter text')
+    path_save = input('Куда сохранить?')
+
+    if path_save == 'yadisk':
+        folder = create_folder(text)
+        yadisk = save_yadisk(folder, text)
+        print('Сохранено на Ядиск')
+    else:
+        locdisk = save_loc(text)
+        print('Сохранено на локальный диск')
+start_prog()
